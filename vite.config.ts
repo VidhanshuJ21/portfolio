ac1746fc89c1db884246947151271874cfa85f28
siddh34 <ssiddharthsutar@gmail.com>
2025-06-29 18:04:51 +0530

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy  } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        { src: 'data', dest: '' },
+        { src: 'src/assets', dest: 'src' },
+      ]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 })
